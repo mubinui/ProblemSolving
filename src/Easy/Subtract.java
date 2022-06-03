46b4d83c9a9e0fb281f3cbffeb449c5a4f7d4f90
uicmu <mubin41@student.sust.edu>
2022-06-03 17:44:33 +0600

    Optimized Substraction Problem

---
+++ src/Easy/Subtract.java
@@ -0,0 +1,80 @@
+package Easy;
+
+import java.util.Scanner;
+
+public class Subtract {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Testing sub_normal method
+
+        int a [] = {5,0,0};
+        for(int i:a){
+            System.out.print(i+" ");
+        }
+        int digit = 1;
+//        a = sub_normal(a,digit);
+//        System.out.println();
+
+//        for(int i:a){
+//            System.out.print(i+" ");
+//        }
+
+//        System.out.println("------------------------------------");
+        //Testing optimized the optimized
+        System.out.println();
+
+        a = sub_optimized(a,digit);
+        for(int i: a){
+            System.out.print(i+" ");
+        }
+
+
+    }
+
+    public static int [] sub_normal(int [] a, int sub){
+        int answer;
+        double sum = 0;
+        //convert the array to a number
+        int j = a.length-1;
+        for (int i = 0; i <a.length ; i++) {
+            sum = Math.pow(10,j) * a[i] + sum;
+            j--;
+
+        }
+//        System.out.println("Sum ->"+sum);
+        answer = (int)sum-sub;
+//        System.out.println("Answer->"+answer);
+        int temp = 0;
+        for (int i = a.length-1; i>=0; i--) {
+            temp = answer%10;
+            a[i]=temp;
+            answer/=10;
+
+        }
+        return a;
+    }
+
+
+    public static int [] sub_optimized(int a[],int dec){
+        int temp = 0;
+        for (int i = a.length-1; i>=0 ; i--) {
+            if(a[i]>(dec+temp)){
+                a[i]=a[i]-(dec+temp);
+                temp = 0;
+                dec = 0;
+            }
+            else{
+                a[i]= (a[i]+10) - (dec+temp);
+                temp = 1;
+                dec = 0;
+
+            }
+
+        }
+
+
+
+        return a;
+
+    }
+}
